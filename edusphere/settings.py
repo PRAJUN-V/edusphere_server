cb9990405bfd169325bd2012ff461983ff35598a
prajun v <prajunvelandi@gmail.com>
2024-10-12 11:05:35 +0530

    s3 bucket changed

--- edusphere/settings.py
+++ edusphere/settings.py
@@ -219,13 +219,13 @@ STRIPE_WEBHOOK_SECRET = 'your-webhook-secret'
 
 SITE_ID = 1
 
-# for setting s3 bucket
+# S3 Bucket settings
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = 'eduspherebackend'
-AWS_S3_SIGNATURE_NAME = 's3v4',
-AWS_S3_REGION_NAME = 'us-east-1'
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_SIGNATURE_NAME = os.getenv('AWS_S3_SIGNATURE_NAME')
+AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME')
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL =  None
 AWS_S3_VERITY = True
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'   
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'  
